72efe6e4113b72a613283f4ddf0503018be47a73
Jack Frate <jack@noEmail.com>
2019-09-06 02:27:43 -0400

    add mobile mode checking root component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,7 @@
 <template>
+<!-- contains a layout for both mobile and -->
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    
   </div>
 </template>
 
---
+++ src/components/RootComponent.vue
@@ -0,0 +1,24 @@
+/**
+ * Root component for the website
+ * Determines if we are in mobile mode or not
+ */
+<template>
+  <div class="root">
+    <div v-if="isMobile()" class="app-mobile"></div>
+    <div v-else class="app-desktop"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({})
+export default class RootComponent extends Vue {
+  /**
+   * checks if the app is in mobile or not
+   */
+  public isMobile(): boolean {
+    return window.length < 966;
+  }
+}
+</script>
